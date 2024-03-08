eacc386dcbe087bef73f835909d16d46f0b9dc5e
SUHAIB SABIR <suhaibsabir110@gmail.com>
2024-03-07 21:59:26 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-// function average(sub1, sub2, sub3) {
+function average(sub1, sub2, sub3) {
 
-//     return sub1 + sub2 + sub3 / 3;
+    return sub1 + sub2 + sub3 / 3;
 
-// }
+}
 
 
-// function percen(totalmarks, scored) {
+function percen(totalmarks, scored) {
 
-//     return (totalmarks / (scored * 100)) * 100;
+    return (totalmarks / (scored * 100)) * 100;
 
-// }
+}
 
-// function main(sub1, sub2, sub3) {
+function main(sub1, sub2, sub3) {
 
-//     var avag = average(sub1, sub2, sub3)
-//     // var percent = percen(totalmarks, scored)
-//     var total = sub1 + sub2 + sub3
-//     // console.log(avag(10, 20, 30))
-// }
+    var avag = average(sub1, sub2, sub3)
+    // var percent = percen(totalmarks, scored)
+    var total = sub1 + sub2 + sub3
+    // console.log(avag(10, 20, 30))
+}
 
-// main(2, 3, 4);
+main(2, 3, 4);
 
 // console.log("dj")
 
@@ -33,17 +33,21 @@ function avarage(sub1, sub2, sub3) {
 
 function percentage(totalMarks, scoredMarks) {
 
-    return (scoredMarks / totalMarks) * 100
+    return (scoredMarks / totalMarks)
 
 }
 
 function main(sub1, sub2, sub3) {
 
     var avag = avarage(sub1, sub2, sub3)
-    var pern = percentage(totalMarks, scoredMarks)
-    totalMarks = sub1 + sub2 + sub3
+    var total = sub1 + sub2 + sub3
+    var pern = percentage(total, total * 3)
+
+    console.log("Average Marks: " + avag);
+    console.log("Percentage: " + pern + "%");
 
 }
+console.log(main(20, 40, 3))
 
 
 

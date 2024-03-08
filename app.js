cd954c8256f14ab6ebaddf3ecfcf4625cfeaab9c
SUHAIB SABIR <suhaibsabir110@gmail.com>
2024-03-07 21:49:57 -0800

    ss
    dd

---
+++ app.js
@@ -0,0 +1,53 @@
+// function average(sub1, sub2, sub3) {
+
+//     return sub1 + sub2 + sub3 / 3;
+
+// }
+
+
+// function percen(totalmarks, scored) {
+
+//     return (totalmarks / (scored * 100)) * 100;
+
+// }
+
+// function main(sub1, sub2, sub3) {
+
+//     var avag = average(sub1, sub2, sub3)
+//     // var percent = percen(totalmarks, scored)
+//     var total = sub1 + sub2 + sub3
+//     // console.log(avag(10, 20, 30))
+// }
+
+// main(2, 3, 4);
+
+// console.log("dj")
+
+
+function avarage(sub1, sub2, sub3) {
+
+    return (sub1 + sub2 + sub3) / 3
+
+
+}
+
+function percentage(totalMarks, scoredMarks) {
+
+    return (scoredMarks / totalMarks) * 100
+
+}
+
+function main(sub1, sub2, sub3) {
+
+    var avag = avarage(sub1, sub2, sub3)
+    var pern = percentage(totalMarks, scoredMarks)
+    totalMarks = sub1 + sub2 + sub3
+
+}
+
+
+
+
+
+
+
